3680daa9b5515af1edba51b937ac8570ea26c80a
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2021-04-19 16:00:55 +0800

    refactor(Config): better default in Config

--- CoolQAPI/utils/config.py
+++ CoolQAPI/utils/config.py
@@ -2,30 +2,46 @@
 import os
 import yaml
 
+from .constant import FilePath
+
 
 class Config:
     def __init__(self, file_path):
-        self.data = None
         self.file_path = file_path
+        self.data = None
+        self.default = {
+            'post_host': '127.0.0.1',
+            'post_port': 5701,
+            'post_path': 'post',
+            'api_host': '127.0.0.1',
+            'api_port': 5700,
+            'command_prefix': '/',
+        }
+        if not os.path.isdir(FilePath.CONFIG_DIR):
+            os.mkdir(FilePath.CONFIG_DIR)
+        self._check()
+
+    def _check(self):
+        self._load()
+        save_flag = False
+        for key, value in self.default.items():
+            if key not in self.data.keys():
+                self.data[key] = value
+                save_flag = True
+        if save_flag:
+            self._save()
+
+    def _load(self):
         if os.path.isfile(self.file_path):
-            with open(self.file_path, encoding='utf8') as file:
-                self.data = yaml.safe_load(file)
-        self.check_config()
+            with open(self.file_path, encoding='utf-8') as f:
+                self.data = yaml.safe_load(f)
+        else:
+            self.data = self.default
+            self._save()
 
-    def touch(self, key, default):
-        if self.data is None:
-            self.data = {}
-        if key not in self.data:
-            self.data[key] = default
-            with open(self.file_path, 'a', encoding='utf8') as file:
-                yaml.safe_dump({key: default}, file)
-
-    def check_config(self):
-        self.touch('post_host', '127.0.0.1')
-        self.touch('post_port', 5701)
-        self.touch('api_host', '127.0.0.1')
-        self.touch('api_port', 5700)
-        self.touch('command_prefix', '/')
+    def _save(self):
+        with open(self.file_path, 'w', encoding='utf-8') as f:
+            yaml.dump(self.data, f)
 
     def __getitem__(self, item):
         return self.data[item]
